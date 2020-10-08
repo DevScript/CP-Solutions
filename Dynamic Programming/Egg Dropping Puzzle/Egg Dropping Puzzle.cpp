1771b83fc437b10ae2d346dd66e9bbf7b2e684fd
Toulik Das <dastoulik12@gmail.com>
2020-10-08 09:23:42 +0530

    Create Egg Dropping Puzzle.cpp

---
+++ Dynamic Programming/Egg Dropping Puzzle/Egg Dropping Puzzle.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <limits.h>
+using namespace std;
+
+int res(int n, int f) 
+{ 
+   
+    int dp[n + 1][f + 1]; 
+    int res; 
+   
+  
+   
+    for (int i = 1; i <= n; i++) { 
+        dp[i][1] = 1; 
+        dp[i][0] = 0; 
+    } 
+  
+   
+    for (int j = 1; j <= f; j++) 
+        dp[1][j] = j; 
+  
+    
+    for (int i = 2; i <= n; i++) { 
+        for (int j = 2; j <= f; j++) { 
+            dp[i][j] = INT_MAX; 
+            for (int x = 1; x <= j; x++) { 
+                res = 1 + max( 
+                              dp[i - 1][x - 1], 
+                              dp[i][j - x]); 
+                if (res < dp[i][j]) 
+                    dp[i][j] = res; 
+            } 
+        } 
+    } 
+  
+    
+    return dp[n][f]; 
+} 
+
+
+int main() {
+    
+    	int n = 2;
+
+		int f = 10;
+    	cout<<res(n, f);
+    	
+    	return 0;
+}
